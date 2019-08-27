b854768ae6bb5c592ddbe2639d89db52adcc1294
web-du <1605993932@qq.com>
2019-08-27 22:37:02 +0800

    新增webpack-dev-server配置与html-webpack-plugin插件介入

--- 01搭建环境/webpack.dev.config.js
+++ 01搭建环境/webpack.dev.config.js
@@ -1,7 +1,25 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
 module.exports = {
     entry: './src/index.js',
     output: {
         path: __dirname,
         filename: './release/bundle.js'
+    },
+    plugins: [
+        // 会把打包之后的文件插入当前配置的模板中运行
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+    // 安装完webpack-dev-Server后再配置devServer
+    // 在webpack的官网指南模块开发下可以查看具体的意思
+    // 最后可以添加一个 script 脚本，可以直接运行开发服务器(dev server)：
+    devServer: {
+        contentBase: path.join(__dirname,'./release'), //根目录：告诉开发服务器(dev server)，在哪里查找文件：
+        open: true, // 自动打开浏览器
+        port: 9000  // 端口号
     }
 }
