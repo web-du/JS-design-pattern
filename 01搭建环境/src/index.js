9d87633d0bac7be1f2f7cc849ccd9183b0eacc3c
web-du <1605993932@qq.com>
2019-08-31 17:55:42 +0800

    准备工作完毕

--- 01搭建环境/src/index.js
+++ 01搭建环境/src/index.js
@@ -1,12 +1,72 @@
+//1.继承
+// 父类
 class Person {
-    constructor (name) {
+    constructor(name, age) {
         this.name = name;
+        this.age = age;
     }
 
-    getName() {
-        return this.name
+    eat() {
+        console.log(`${this.name} eat something`)
+    }
+
+    speak() {
+        console.log(`My name is ${this.name}, age ${this.age}`)
     }
 }
 
-let p = new Person('dwz');
-console.log(p.getName())
+// 子类
+class Student extends Person {
+    constructor(name, age, number) {
+        // super:执行父类的构造方法
+        super(name, age)
+        this.number = number
+    }
+    study() {
+        console.log(`${this.name} study`)
+    }
+}
+
+
+//实例
+let xiaoming = new Student('xiaoming', 10, 'A1')
+xiaoming.study()
+console.log(xiaoming.number)
+xiaoming.eat()
+
+let xiaohong = new Student('xiaohong', 11, 'A2')
+xiaohong.study()
+xiaohong.speak()
+
+
+
+// jq
+
+class jQuery {
+    constructor(seletor) {
+        let slice  = Array.prototype.slice;
+        // 伪数组转真数组
+        let dom = slice.call(document.querySelectorAll(seletor))
+        let len = dom ? dom.length : 0
+        for (let i = 0; i < len ; i++) {
+            this[i] = dom[i]
+        }
+        this.length = len
+        this.seletor = seletor || ''
+    }
+    append(node) {
+
+    }
+    addClass(name) {
+    }
+    html(data) {
+
+    }
+}
+
+window.$ = function(seletor) {
+    return new jQuery(seletor)
+}
+
+var p = $('p');
+console.log(p.constructor)
