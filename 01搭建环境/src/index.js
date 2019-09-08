b7cbfc1d2788b1af6c703c12d4bf2e02da3968fb
web-du <1605993932@qq.com>
2019-09-08 21:43:07 +0800

    单例模式学习

--- 01搭建环境/src/index.js
+++ 01搭建环境/src/index.js
@@ -1,37 +1,78 @@
-// 工厂模式
+// 单例模式
+// class SingleObject {
+//     login() {
+//         console.log('login...');
+//     }
+// }
 
-class Product {
-    constructor(name) {
-        this.name = name
+// // 静态方法
+// SingleObject.getInstance = (function () {
+//     let instance
+
+//     return function () {
+
+//         // 单例模式的实现思想：有就直接返回，没有就初始化
+//         if (!instance) {
+//             instance = new SingleObject();
+//         }
+
+//         return instance;
+//     }
+// })()
+
+// let obj1 = SingleObject.getInstance();
+// obj1.login();
+
+// let obj2 = SingleObject.getInstance();
+// obj2.login();
+
+// console.log(obj2 === obj1) // true 单例模式中所有的login都是一样的，只有一个
+
+
+
+// 登录
+class LoginForm {
+    constructor() {
+        this.state = 'hide'
     }
-    init() {
-        console.log('init');
+
+    show() {
+        if (this.state === 'show') {
+            console.log('已经显示');
+            return;
+        }
+
+        this.state = 'show';
+        console.log('显示成功')
     }
-    func1() {
-        console.log('func1');
-    }
-    func2() {
-        console.log('func2')
+
+    hide() {
+        if (this.state === 'hide') {
+            console.log('已经隐藏');
+            return;
+        }
+
+        this.state = 'hide';
+        console.log('隐藏成功');
     }
 }
 
+//登录的单例
+LoginForm.getInstance = (function() {
+    let islogin
 
-class Create {
-    create(name) {
-        return new Product(name)
+    return function () {
+        if (!islogin) {
+            islogin = new LoginForm()
+        }
+        return islogin
     }
-}
-
+})()
 
 // 测试
-let creator = new Create();
-let p = creator.create('p1');
-p.init();
-p.func1();
 
+let login1 = LoginForm.getInstance();
+login1.show();
 
-//场景
-
-// jQuery - $('div');
-// React.createElement
-// vue 异步组件
+let login2 = LoginForm.getInstance()
+login2.show();
